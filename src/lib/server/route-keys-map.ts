e32ec21bbd1493140d71e2b112e13a1c94a75034
Anton Pavlenkov <pavlenkov47@gmail.com>
2025-08-26 13:53:36 +0300

    Update .gitignore to include generated messages and route-keys-map.ts. Initialize RouteKeysMap with predefined routes for improved translation management and maintainability.

--- src/lib/server/route-keys-map.ts
+++ src/lib/server/route-keys-map.ts
@@ -1,6 +1,18 @@
 // // =============================================================================
 // // AUTO-GENERATED CODE BY SVELTEKIT-TRANSLATIONS-LOADER PLUGIN
-const RouteKeysMap = new Map<string, string[]>([]);
+const RouteKeysMap = new Map<string, string[]>([
+	['{route:"/",type:"layout"}', []],
+	['{route:"/",type:"page"}', []],
+	['{route:"/deep-scanning-test",type:"page"}', []],
+	['{route:"/nested-example",type:"layout"}', []],
+	['{route:"/nested-example",type:"page"}', []],
+	['{route:"/some-page",type:"page"}', []],
+	['{route:"/test-empty-file",type:"page"}', []],
+	['{route:"/test-empty-keys",type:"page"}', []],
+	['{route:"/test-lib-imports",type:"page"}', []],
+	['{route:"/some-page/[slug]/more",type:"page"}', []],
+	['{route:"/some-page/[slug]/blog/5.0",type:"page"}', []]
+]);
 // // END AUTO-GENERATED CODE
 
 export default RouteKeysMap;
